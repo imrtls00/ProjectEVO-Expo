1119a6ebb26815cae563557e20d5f8b386c9b39a
imrtls00 <imrtls00@gmail.com>
2025-01-12 17:32:15 +0500

    ✨ Add ESLint configuration and update package.json scripts; improve code quality tools

--- android/app/src/main/java/com/imrtls00/projectevo/MainApplication.kt
+++ android/app/src/main/java/com/imrtls00/projectevo/MainApplication.kt
@@ -14,7 +14,7 @@ import com.facebook.soloader.SoLoader
 
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
-
+ 
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
