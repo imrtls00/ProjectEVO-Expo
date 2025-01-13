8ada4b290e0578124e7d28ecd0b9227cd1214390
imrtls00 <imrtls00@gmail.com>
2025-01-13 12:40:04 +0500

    ↩️ revert back to old input field style

--- src/components/InputSection.tsx
+++ src/components/InputSection.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { View, TextInput, Pressable, ActivityIndicator, Text } from "react-native";
+import { View, TextInput, Pressable, StyleSheet, ActivityIndicator, Text } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { globalStyles, colors } from '@/src/Styles/globalStyles';
 
 const InputSection = ({ inputValue, onChange, onMicPress, onSend, recognizing, loading }: any) => (
-  <View style={globalStyles.container}>
+  <View style={styles.inputContainer}>
     <TextInput
       style={globalStyles.input}
       placeholder="What do you wanna do?"
@@ -19,7 +19,7 @@ const InputSection = ({ inputValue, onChange, onMicPress, onSend, recognizing, l
         <Text style={globalStyles.loaderText}>Engine is evaluating...</Text>
       </View>
     ) : (
-      <Pressable style={globalStyles.button}>
+      <Pressable style={styles.submitButton}>
         {inputValue ? (
           <MaterialIcons name="send" size={24} color={colors.TextHighlight} onPress={onSend} />
         ) : recognizing ? (
@@ -31,5 +31,30 @@ const InputSection = ({ inputValue, onChange, onMicPress, onSend, recognizing, l
     )}
   </View>
 );
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderColor: colors.Border,
+    borderWidth: 1,
+    borderRadius: 100,
+    paddingLeft: 32,
+    paddingRight: 4,
+    paddingVertical: 4,
+    backgroundColor: colors.BackgroundSecondary,
+  },
+  input: {
+    flex: 1,
+    color: "#F0F0F0",
+    fontSize: 18,
+  },
+  submitButton: {
+    backgroundColor: "#0061FF",
+    padding: 16,
+    borderRadius: 30,
+    marginLeft: 10,
+  },
+});
 
 export default InputSection;
